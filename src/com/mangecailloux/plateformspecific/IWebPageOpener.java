603bc850f8a9b8e0989aeb9c9f40e44b7c25f881
Clément Vayer <cvayer@gmail.com>
2012-12-19 19:19:54 +0100

    Change webPageOpener, made a desktop version

--- src/com/mangecailloux/plateformspecific/IWebPageOpener.java
+++ src/com/mangecailloux/plateformspecific/IWebPageOpener.java
@@ -1,5 +1,14 @@
 package com.mangecailloux.plateformspecific;
 
+
+
 public interface IWebPageOpener {
-	public void open(String _url);
+	
+	public enum EMode
+	{
+		eURL,
+		eMarket
+	};
+	
+	public void open(String _url, EMode _mode);
 }
